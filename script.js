f80551c2900a022fc76800342da21c1bffea0f1f
Subhadeep25 <84439620+Subhadeeep25@users.noreply.github.com>
2022-06-17 18:35:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function computeint() {
+  var p = document.getElementById("principal").value;
+  var r = document.getElementById("rate").value;
+  var t = document.getElementById("years").value;
+  var si = (p * t * r) / 100;
+  var total = p + si;
+  console.log(si);
+  console.log(p);
+  console.log(r);
+  console.log(total);
+  document.getElementById("amt").innerHTML = p;
+  document.getElementById("intr").innerHTML = r;
+  document.getElementById("tot").innerHTML = total;
+  document.getElementById("ys").innerHTML = 2022 + t;
 }
+
+function setup() {
+  document.getElementById("rt").innerHTML = document.getElementById(
+    "rate"
+  ).value;
+}
+
         
